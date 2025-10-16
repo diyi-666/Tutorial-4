4fa87cc951fd285a07c5f89a58c9c188405030cf
diyi-666 <2085046784@qq.com>
2025-10-16 15:41:28 +0800

    Implement subtract method

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,18 +1,11 @@
 package org.example;
 
 public class Calc {
-
-    public int add(int x, int y)
-    {
-        return x+y;
+    public int add(int x, int y) {
+        return x + y;
     }
 
-    public int subtract(int x, int y)
-    {
-        return x*y;
+    public int subtract(int x, int y) {
+        return x - y;
     }
-}
-
-public int subtract(int a, int b) {
-    return a - b;
 }
